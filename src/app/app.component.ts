001e450cfe1e78a0ca699b5e2e2e4af68a6536f9
niroberto <robertwilly668@gmail.com>
2025-07-11 20:20:18 +0200

    Ft/ update content  localization and translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CoursesComponent, NgFor],
+  imports: [
+    RouterOutlet,
+    CoursesComponent,
+    NgFor,
+    RouterLink,
+    RouterLinkActive,
+    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
